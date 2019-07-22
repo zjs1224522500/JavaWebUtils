b3f90706b42607b3f56e6973c588fac7ff1ad89a
Elvis <1224522500@qq.com>
2019-07-22 19:20:28 +0800

    Add log split util

---
+++ CommonUtils/src/main/java/tech/shunzi/model/utils/SplitLog.java
@@ -0,0 +1,19 @@
+package tech.shunzi.model.utils;
+
+public class SplitLog {
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\lyh\\Desktop\\log.txt";
+        String content = FileUtils.readFile(filePath);
+        String splitString = "2019-07-22";
+
+        for (String singleLog : content.split(splitString))
+        {
+            if (singleLog.contains("Read file with length"))
+            {
+                System.out.println();
+            }
+            System.out.println(splitString + singleLog);
+        }
+    }
+}
